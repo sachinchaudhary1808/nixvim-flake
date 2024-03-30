4ee7fb27990bfb25723a12e51aae07b02db85036
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2024-03-30 16:00:36 +0530

    added

--- config/nixvim.nix
+++ config/nixvim.nix
@@ -23,18 +23,50 @@ in {
     # inputs.nixvim.nixosModules.nixvim
   ];
   config = {
-    colorschemes.gruvbox.enable = true;
-    #colorschemes.catppuccin = {
-    #     enable = true;
-    #    flavour = "mocha";
-    # transparentBackground = true;
-    #    };
+    # colorschemes.gruvbox.enable = true;
+    colorschemes.catppuccin = {
+      enable = true;
+      flavour = "mocha";
+      transparentBackground = true;
+    };
 
     opts = {
       number = true; # Show line numbers
       relativenumber = true; # Show relative line numbers
 
-      shiftwidth = 2; # Tab width should be 2
+      # autoindent = true;
+      # backspace = "indent,eol,start";
+      # backup = true;
+      cmdheight = 1;
+      colorcolumn = "80";
+      /*
+      completeopt = "menu,menuone,noselect";
+      */
+      #
+      # conceallevel = 0;
+      # cursorline = true;
+      # expandtab = true;
+      # foldenable = true;
+      # foldexpr = "nvim_treesitter#foldexpr()";
+      # foldlevel = 5;
+      # foldmethod = "expr";
+      # ignorecase = true;
+      # laststatus = 3;
+      # mouse = "a";
+      /*
+      pumheight = 0;
+      */
+      shiftwidth = 4;
+      showtabline = 1;
+      signcolumn = "yes";
+      smartcase = true;
+      tabstop = 4;
+      termguicolors = true;
+      timeoutlen = 300;
+      # undofile = true;
+      # updatetime = 300;
+      wrap = false;
+      # writebackup = true;
       clipboard = "unnamedplus";
     };
 
