798c40fdd017bc0058b8a7bc6600ea1dc3cacc80
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2024-05-02 16:55:32 +0530

    updated

--- config/nixvim.nix
+++ config/nixvim.nix
@@ -28,13 +28,15 @@ in {
     # };
     #
     # colorschemes.tokyonight.enable = true;
-    colorschemes.catppuccin = {
-      enable = true;
-      settings = {
-        flavour = "mocha";
-      };
-      # transparentBackground = true;
-    };
+    # colorschemes.catppuccin = {
+    #   enable = true;
+    #   settings = {
+    #     flavour = "mocha";
+    #   };
+    #   # transparentBackground = true;
+    # };
+
+    colorschemes.kanagawa.enable = true;
 
     opts = {
       number = true; # Show line numbers
@@ -75,20 +77,46 @@ in {
       clipboard = "unnamedplus";
     };
 
+
+     plugins.lspkind = {
+    enable = true;
+    symbolMap = {
+      Copilot = "";
+    };
+    extraOptions = {
+      maxwidth = 50;
+      ellipsis_char = "...";
+    };
+  };
+
     plugins = {
       wtf.enable = true;
+      friendly-snippets.enable = true;
+      oil.enable = true;
+      lazygit.enable = true;
       surround.enable = true;
+      # dressing.enable = true;
       indent-blankline.enable = true;
       lualine.enable = true;
       none-ls.enable = true;
       toggleterm.enable = true;
       which-key.enable = true;
+      bufferline = {
+        enable = true;
+        alwaysShowBufferline = false;
+      };
       alpha.enable = true;
       alpha.theme = "startify";
       # noice.enable = true;
       vim-css-color.enable = true;
       # notify.enable = true;
-      luasnip.enable = true; # not configured
+      luasnip = {
+        enable = true;
+        extraConfig = {
+          enable_autosnippets = true;
+          store_selection_keys = "<Tab>";
+        };
+      }; # not configured
       # plugins.startup.enable = true;
       nvim-colorizer.enable = true;
       leap.enable = true;
@@ -118,13 +146,33 @@ in {
         };
       };
 
+      cmp-cmdline.enable = true;
       cmp-nvim-lsp.enable = true;
       cmp = {
         enable = true;
+        autoEnableSources = true;
         settings = {
+          snippet = {expand = "luasnip";};
+
           sources = [
             {name = "nvim_lsp";}
+            {name = "path";}
+            {name = "buffer";}
+            {name = "luasnip";}
+            {name = "cmdline";}
           ];
+
+          mapping = {
+            "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+            "<C-j>" = "cmp.mapping.select_next_item()";
+            "<C-k>" = "cmp.mapping.select_prev_item()";
+            "<C-e>" = "cmp.mapping.abort()";
+            "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+            "<C-f>" = "cmp.mapping.scroll_docs(4)";
+            "<C-Space>" = "cmp.mapping.complete()";
+            "<CR>" = "cmp.mapping.confirm({ select = true })";
+            "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
+          };
         };
       };
     };
